e2f33f525c7cfacee16a067507807639154689db
oakoak <dubizhanskii@mail.ru>
2022-12-01 00:24:04 +0300

    add processind errors 404, 500

--- app/forms.py
+++ app/forms.py
@@ -4,7 +4,6 @@ from wtforms.validators import DataRequired, ValidationError, Email, EqualTo, Le
 from app.models import User
 
 
-
 class LoginForm(FlaskForm):
     username = StringField('Username', validators=[DataRequired()])
     password = PasswordField('Password', validators=[DataRequired()])
